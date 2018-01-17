89d68bc539d8dd47b1dd72b1f19d494af5d56ebe
xiake_sun <sammysun0711@gmail.com>
2018-01-18 00:10:54 +0100

    Update pi.cmake

--- pi.cmake
+++ pi.cmake
@@ -1,23 +1,11 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
-
 SET(CMAKE_C_COMPILER $ENV{HOME}/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
 SET(CMAKE_CXX_COMPILER $ENV{HOME}/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-g++)
+SET(CMAKE_SYSROOT $ENV{HOME}/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot)
 SET(CMAKE_FIND_ROOT_PATH $ENV{HOME}/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot)
+
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-#set(CMAKE_SYSROOT ${RASPI_SYSROOT})
-#    include_directories(${RASPI_SYSROOT}/usr/include/arm-linux-gnueabihf)
-#set(CMAKE_CXX_FLAGS "-isystem /home/sammysun/raspberrypi/rootfs/usr/include/arm-linux-gnueabihf")
-
-add_definitions(-Wall -std=c++11)
-include_directories({$HOME/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/opt/vc/include})
-include_directories({$HOME/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/opt/vc/include/interface/vcos
-})
-include_directories({$HOME/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/opt/vc/include/interface/vcos/pthreads
-})
-include_directories({$HOME/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/opt/vc/include/interface/vmcs_host/linux
-})
-
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
